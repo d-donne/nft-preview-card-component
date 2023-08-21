15ccd74ac80d092dbf1167b6cb1a2b5c3fa8c97a
@d-donne <doublyd@gmail.com>
2023-08-21 18:28:36 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      "soft-blue": "hsl(215, 51%, 70%)",
+      cyan: "hsl(178, 100%, 50%)",
+      "very-dark-blue": "hsl(217, 54%, 11%)",
+      "very-dark-blue": "hsl(216, 50%, 16%)",
+      "Very-dark-blue": "hsl(215, 32%, 27%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+    fontFamily: {
+      "ff-primary": ["Outfit", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
