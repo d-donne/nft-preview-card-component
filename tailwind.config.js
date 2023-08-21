e64772640d8c35e10792425027665b4bf97cd316
@d-donne <doublyd@gmail.com>
2023-08-21 19:44:52 +0000

    finished layout and primary styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*html"],
   theme: {
     extend: {},
     colors: {
       "soft-blue": "hsl(215, 51%, 70%)",
       cyan: "hsl(178, 100%, 50%)",
-      "very-dark-blue": "hsl(217, 54%, 11%)",
-      "very-dark-blue": "hsl(216, 50%, 16%)",
-      "Very-dark-blue": "hsl(215, 32%, 27%)",
+      "bg-primary": "hsl(217, 54%, 11%)",
+      "bg-card": "hsl(216, 50%, 16%)",
+      "very-dark-blue": "hsl(215, 32%, 27%)",
       white: "hsl(0, 0%, 100%)",
     },
     fontFamily: {
